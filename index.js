8e0588d15e00f0d95e109d3dbdbceae43decb445
TheStackMaster <unijoy84@gmail.com>
2025-07-07 20:26:54 +0600

    Adding all the folders and files for the project

---
+++ index.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require("cors")
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+
+        app.get("/", (req, res) => {
+            res.send("Server is running")
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+    console.log("Server is running on", port)
+})
