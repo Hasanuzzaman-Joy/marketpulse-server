834ac1c7423d5e1f5b400d4fbd141de2aee000cf
TheStackMaster <unijoy84@gmail.com>
2025-07-09 17:35:33 +0600

    Deploy backend API to Vercel

--- index.js
+++ index.js
@@ -21,17 +21,17 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        await client.connect();
+        // await client.connect();
 
         app.get("/", (req, res) => {
             res.send("Server is running")
         })
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
